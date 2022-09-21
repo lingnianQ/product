00cfb9b6435d647a20a73a847628eb28944cf26e
lingnian <sytsnb@gmail.com>
2022-09-21 14:54:55 +0800

    init

---
+++ src/test/java/com/syt/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syt.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
