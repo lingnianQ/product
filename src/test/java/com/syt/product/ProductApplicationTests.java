98e0677f9985cc941a8a1d0134036640fdb778f9
lingnian <sytsnb@gmail.com>
2022-09-21 17:36:11 +0800

    init

--- src/test/java/com/syt/product/ProductApplicationTests.java
+++ src/test/java/com/syt/product/ProductApplicationTests.java
@@ -1,13 +1,26 @@
 package com.syt.product;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.sql.DataSource;
+
 @SpringBootTest
+@Slf4j
 class ProductApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testConnection() throws Throwable {
+        dataSource.getConnection();
+        log.info("连接成功");
+    }
 }
