b7cd49601f1f28309c2b5d7e15a08a7f1bc8062d
lingnian <sytsnb@gmail.com>
2022-09-26 15:49:17 +0800

    SkuMapper

--- src/main/java/com/syt/product/mapper/SkuMapper.java
+++ src/main/java/com/syt/product/mapper/SkuMapper.java
@@ -1,6 +1,10 @@
 package com.syt.product.mapper;
 
 import com.syt.product.pojo.entity.Sku;
+import com.syt.product.pojo.vo.SkuListItemVO;
+import com.syt.product.pojo.vo.SkuStandardVO;
+
+import java.util.List;
 
 /**
  * 处理SKU数据的Mapper接口
@@ -8,26 +12,66 @@ import com.syt.product.pojo.entity.Sku;
  * @author sytsn
  */
 public interface SkuMapper {
+
     /**
-     * 插入
+     * 插入SKU数据
      *
-     * @param sku
-     * @return
+     * @param sku SKU数据
+     * @return 受影响的行数
      */
     int insert(Sku sku);
 
     /**
-     * 根据主键id删除
+     * 批量插入SKU数据
      *
-     * @param id
-     * @return
+     * @param skuList 若干个SKU数据的集合
+     * @return 受影响的行数
+     */
+    int insertBatch(List<Sku> skuList);
+
+    /**
+     * 根据id删除SKU数据
+     *
+     * @param id SKUid
+     * @return 受影响的行数
      */
     int deleteById(Long id);
 
     /**
-     * 计数
+     * 批量删除SKU
      *
-     * @return
+     * @param ids 需要删除的若干个SKU的id
+     * @return 受影响的行数
+     */
+    int deleteByIds(Long[] ids);
+
+    /**
+     * 更新SKU数据
+     *
+     * @param sku 封装了SKU的id和需要更新的新数据的对象
+     * @return 受影响的行数
+     */
+    int updateById(Sku sku);
+
+    /**
+     * 统计SKU数据的数量
+     *
+     * @return SKU数据的数量
      */
     int count();
+
+    /**
+     * 根据id查询SKU标准信息
+     *
+     * @param id SKUid
+     * @return 匹配的SKU的标准信息，如果没有匹配的数据，则返回null
+     */
+    SkuStandardVO getStandardById(Long id);
+
+    /**
+     * 查询SKU列表
+     *
+     * @return SKU列表
+     */
+    List<SkuListItemVO> list();
 }
--- src/test/java/com/syt/product/mapper/SkuMapperTest.java
+++ src/test/java/com/syt/product/mapper/SkuMapperTest.java
@@ -1,11 +1,15 @@
 package com.syt.product.mapper;
 
 import com.syt.product.pojo.entity.Sku;
+import com.syt.product.pojo.vo.SkuStandardVO;
 import com.syt.product.util.IdUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,14 +29,59 @@ class SkuMapperTest {
     }
 
     @Test
+    void testInsertBatch() {
+        List<Sku> skuList = new ArrayList<>();
+        for (int i = 1; i < 19; i++) {
+            Sku sku = new Sku();
+            sku.setId(IdUtils.getId());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            skuList.add(sku);
+        }
+        int rows = skuMapper.insertBatch(skuList);
+        System.out.println("rows = " + rows);
+    }
+
+    @Test
     void testDeleteById() {
         int i = skuMapper.deleteById(IdUtils.getId());
         System.out.println("i = " + i);
     }
 
     @Test
+    void testDeleteByIds() {
+        Long[] ids = {2L, 3L, 4L};
+        int rows = skuMapper.deleteByIds(ids);
+        System.out.println("rows = " + rows);
+    }
+
+
+    @Test
+    void testUpdateById() {
+        Sku sku = new Sku();
+        sku.setId(9L);
+        sku.setTitle("testUpdate");
+        int rows = skuMapper.updateById(sku);
+        System.out.println("rows = " + rows);
+    }
+
+    @Test
     void testCount() {
         int count = skuMapper.count();
         System.out.println("count = " + count);
+    }
+
+    @Test
+    void testGetStandardById() {
+        SkuStandardVO skuStandardVO = skuMapper.getStandardById(9L);
+        System.out.println("skuStandardVO = " + skuStandardVO);
+    }
+
+    @Test
+    void testList() {
+        skuMapper.list().forEach(System.out::println);
     }
 }
